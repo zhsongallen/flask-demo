a5b9075f36bf99d193374b995369fceac1dd912e
zhsongallen <60937247+zhsongallen@users.noreply.github.com>
2020-05-18 01:13:26 +0300

    Create schema for graphql version of my demo

--- schema.py
+++ schema.py
@@ -28,14 +28,28 @@ class Dealer(SQLAlchemyObjectType):
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    all_customers = SQLAlchemyConnectionField(Customer.connection)
-    all_dealers = SQLAlchemyConnectionField(Dealer.connection)
+    
+    #/getAllCustomers
+    all_Customers = graphene.List(Customer)
+    def resolve_all_Customers(self, info):
+        query = Customer.get_query(info)
+        return query
+    
+    #/getAllDealers
+    all_Dealers = graphene.List(Dealer)
+    def resolve_all_Dealers(self, info):
+        query = Dealer.get_query(info)
+        return query
+    
+    #Similar to previous routes
     all_orders = SQLAlchemyConnectionField(Order.connection)
     all_feedbacks = SQLAlchemyConnectionField(Feedback.connection)
+
+    #/getCustomerByName
     customers = graphene.List(Customer, name=graphene.String())
     def resolve_customers(self, info, **args):
         query = Customer.get_query(info)
         print(args.get("name"))
         return query.filter(CustomerModel.name == args.get('name'))
-
+        
 schema = graphene.Schema(query=Query)
